14141a9d29275c10d16d879bc30a506181303515
mmengelbier <magnus.mengelbier@gmail.com>
2025-05-31 10:22:33 +0000

    adding code for azure block blob store

--- R/txflow_store.R
+++ R/txflow_store.R
@@ -1,6 +1,6 @@
 #' Utility function to connect the txflow storage configuration
 #' 
-#' @return A tx flow storage object
+#' @return A txflow storage object
 #' 
 #' @export
 
@@ -14,7 +14,10 @@ txflow_store <- function() {
   if ( base::tolower(base::trimws(cfg$option("txflow.store", unset = "not-defined"))) == "local" )
     return(invisible( txflow.service:::.txflow_fsstore() ))
     
-    
+  # -- Azure block blob storage
+  if ( base::tolower(base::trimws(cfg$option("txflow.store", unset = "not-defined"))) == "azureblobs" )
+    return(invisible( txflow.service:::.txflow_azureblobs ))
+  
     
   stop( "Invalid storage configuration")
 }
